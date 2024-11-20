e60c44866bd57625a8aedad3fa0b70f3eb9ebbf9
toki513 <tokitahmidkhan@gmail.com>
2024-11-20 09:42:12 +0600

    updated

---
+++ phone.js
@@ -0,0 +1,19 @@
+
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+
+    const data = await res.json()
+
+    const phones = data.data
+
+    displayPhones(phones)
+}
+
+const displayPhones = phones =>{
+  phones.forEach(phone =>{
+    console.log(phone)
+    
+  })
+}
+
+loadPhone()
